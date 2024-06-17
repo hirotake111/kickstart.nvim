cb28cdab076b490ddda42781ed4058624333852c
hirotake111 <hirotake111@users.noreply.github.com>
2024-06-18 07:27:11 +0900

    LSP for docker-compose

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -1,4 +1,14 @@
 local lspconfig = require 'lspconfig'
 
 lspconfig.gleam.setup {}
+
+lspconfig.docker_compose_language_service.setup {}
+-- A workaround for not detecting docker-compose.yaml file type correctly
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+  pattern = { 'docker-compose.yaml', 'docker-compose.yml' },
+  callback = function()
+    vim.bo.filetype = 'yaml.docker-compose'
+  end,
+})
+
 return {}
