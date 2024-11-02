ebcdbd830e65b583c31b5d1bf8d05ef4a8877878
hirotake111 <hirotake111@users.noreply.github.com>
2024-11-02 15:54:25 +0900

    add a new keymap that capitalizes current word

--- plugin/keymap.lua
+++ plugin/keymap.lua
@@ -7,3 +7,11 @@ vim.keymap.set('n', 'L', '$')
 -- test
 vim.keymap.set('n', '<leader>tf', '<Plug>PlenaryTestFile', { desc = '<Plenary>Run [T]est [F]iles' })
 vim.keymap.set('n', '<leader>td', ':PlenaryBustedDirectory .<CR>', { desc = '<Plenary>Run [T]est [D]irectory' })
+
+vim.keymap.set('i', '<C-l>', function()
+  local line = vim.fn.getline '.'
+  local col = vim.fn.getpos('.')[3]
+  local substring = line:sub(1, col)
+  local result = vim.fn.matchstr(substring, [[\v<(\k(<)@!)*$]])
+  return '<C-w>' .. result:upper()
+end, { noremap = true, silent = true, desc = 'CAPITALIZE current word' })
