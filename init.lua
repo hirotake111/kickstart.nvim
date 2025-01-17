9040d18db0eb37d59cd66d61cc915e6db5e16ab5
hirotake111 <hirotake111@users.noreply.github.com>
2025-01-17 16:17:19 +0900

    add keybindings

--- init.lua
+++ init.lua
@@ -192,6 +192,7 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-i>', '<C-I>', { desc = 'Move to the next item in the jumplist' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
