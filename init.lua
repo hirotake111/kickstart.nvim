8ce6dcc10cca7f940b91696d2e32695bdc91b69e
hirotake111 <hirotake111@users.noreply.github.com>
2026-01-15 03:30:08 +0900

    add gitui

--- init.lua
+++ init.lua
@@ -435,12 +435,50 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'folke/snacks.nvim',
+    opts = {
+      terminal = {
+        win = {
+          style = 'terminal',
+          border = 'rounded',
+        },
+      },
+    },
+  },
+
+  -- mason-org/mason
+  {
+    'mason-org/mason.nvim',
+    opts = { ensure_installed = { 'gitui' } },
+    keys = {
+      {
+        '<leader>gg',
+        function()
+          Snacks.terminal { 'gitui' }
+        end,
+        desc = 'GitUi (cwd)',
+      },
+    },
+    init = function()
+      -- delete lazygit keymap for file history
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'LazyVimKeymaps',
+        once = true,
+        callback = function()
+          pcall(vim.keymap.del, 'n', '<leader>gf')
+          pcall(vim.keymap.del, 'n', '<leader>gl')
+        end,
+      })
+    end,
+  },
+
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
-      { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
-      'williamboman/mason-lspconfig.nvim',
+      { 'mason-org/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
+      'mason-org/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
